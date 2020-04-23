587609799fc1776f703457081a2ed9fe5fe8041d
Jacques Joky <jacques.joky@gmail.com>
2020-04-23 15:45:33 -0400

    Fix toggleDrum, implement initialize, clear, invert

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,37 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = [];
+let snares = [];
+let hiHats = [];
+let rideCymbals = [];
+
+const initialize = (drum) => {
+    for(let i = 0; i < 16; i++) {
+        drum.push(false);
+    }
+}
 
 const toggleDrum = (drum, index) => {
     drum[index] = !drum[index];
 }
 
+const clear = drum => {
+    drum.forEach((element, index, array) => {
+        array[index] = false;
+    });
+};
+
+const invert = drum => {
+    drum.forEach((element, index, array) => {
+        array[index] = !element;
+    });
+};
+
+initialize(kicks);
+initialize(snares);
+initialize(hiHats);
+initialize(rideCymbals);
+
+console.log(kicks);
+console.log(snares);
+console.log(hiHats);
+console.log(rideCymbals);
